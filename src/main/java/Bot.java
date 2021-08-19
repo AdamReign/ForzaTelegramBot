6cf902e95c4b028b1dcdd4fe400ca773efe5039f
AdamReign <87915602+AdamReign@users.noreply.github.com>
2021-08-19 23:09:25 +0300

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,26 +1,86 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
     private static final String TOKEN = System.getenv("TOKEN");
     private static final String USERNAME = System.getenv("USERNAME");
-    public Update update;
+    private Update update;
 
     @Override
     public void onUpdateReceived(Update update) {
         this.update = update;
 
-        if (update.getMessage() != null && update.getMessage().hasText()) {
-            String chatId = update.getMessage().getChatId().toString();
+        try {
+            if (update.getMessage() != null && update.getMessage().hasText()) {
+                executeCommand(update.getMessage().getText());
 
-            if (update.getMessage().getText().equals("/start")) {
-                try { execute(new SendMessage(chatId, "Пук")); }
-                catch (TelegramApiException e) { e.printStackTrace(); }
+            } else if (update.hasCallbackQuery()) {
+                responseToRequest(update.getCallbackQuery());
             }
+        } catch (TelegramApiException e) { e.printStackTrace(); }
+    }
 
+    public void executeCommand(String command) throws TelegramApiException {
+        String chatId = update.getMessage().getChatId().toString();
 
+        switch (command) {
+            case "/start":
+                execute(new MessageList().mainMenu(chatId));
+                break;
+
+            case "/send_photo":
+                execute(new SendPhoto(chatId, new InputFile(new File("D:/image.jpg"))));
+                break;
+        }
+
+//        SheetWorker sheetWorker = new SheetWorker();
+//        sheetWorker.remove("key3");
+//        String[] arr = command.split("=");
+//        sheetWorker.add("buttons", Arrays.asList(Arrays.asList(arr[0], arr[1])));
+//        sheetWorker.setValue("buttons", arr[0], arr.length<2 ? "null" : arr[1]);
+//        sheetWorker.deleteRow();
+//        System.out.println(chatId);
+    }
+
+    public void responseToRequest(CallbackQuery callbackQuery) throws TelegramApiException {
+        String chatId = callbackQuery.getMessage().getChatId().toString();
+        int messageId = callbackQuery.getMessage().getMessageId();
+
+        switch (callbackQuery.getData()) {
+            case "main_menu":
+                execute(new MessageList().backToMainMenu(chatId, messageId));
+                break;
+
+            case "products":
+                execute(new MessageList().products(chatId, messageId));
+                break;
+
+            case "products_kf":
+                execute(new MessageList().productsKF(chatId, messageId));
+                break;
+
+            case "products_forza":
+                execute(new MessageList().productsForza(chatId, messageId));
+                break;
+
+            case "logins_and_passwords":
+                execute(new MessageList().loginsAndPasswords(chatId, messageId));
+                break;
+
+            case "faq":
+                execute(new MessageList().faq(chatId, messageId));
+                break;
         }
     }
 
@@ -32,9 +92,5 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return TOKEN;
-    }
-
-    public Bot() {
-
     }
 }
